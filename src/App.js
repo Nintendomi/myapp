71ad01b2776c55978e85a87799899ea2a5368952
Domenico <dagodomenico@gmail.com>
2021-08-06 12:21:09 +0200

    Terza modifica

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Seconda modifica!
+          Edit <code>src/App.js</code> and save to reload. Terza modifica!
         </p>
         <a
           className="App-link"
